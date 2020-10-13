ba5b7f499f0a81e4f0d118e53efc751acd41f1df
wangronghua <2696731533@qq.com>
2020-10-13 17:10:38 +0800

    完成了主页功能的开

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,28 @@
 // 注意，每次调用请求接口的时候会先调用ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function (options) {
     // 同意拼接的跟路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
-    console.log(options.url);
+    // console.log(options.url);
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 comple 回调函数
+    options.complete = function (res) {
+        // console.log('执行了 complete 回调:');
+        // console.log(res);
+        // 在 complete 回调函数中可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空 token
+            localStorage.removeItem('token')
+            // 2.强制跳转到登录页面
+            location.href = 'login.html'
+        }
+    }
+
 })
